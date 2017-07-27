0018cb9bcec5036b2bc80dcbdfc7e516bab17c86
Vince Cima <contact@vincecima.com>
2017-07-26 19:38:57 -0500

    Add Homebrew Formula

---
+++ HomebrewFormula/gifme.rb
@@ -0,0 +1,16 @@
+class Gifme < Formula
+  desc "Wrapper for ffmpeg to ease the creation of high-quality gifs from videos"
+  homepage "https://github.com/scottpham/gifme"
+  url "https://github.com/scottpham/gifme.git",
+    :revision => "456756e9ca7f8a359417a8a91bbfacb7a4b0924b"
+  version "1"
+  head "https://github.com/scottpham/gifme.git"
+
+  bottle :unneeded
+
+  depends_on "ffmpeg"
+
+  def install
+    bin.install "gifme.sh" => "gifme"
+  end
+end
